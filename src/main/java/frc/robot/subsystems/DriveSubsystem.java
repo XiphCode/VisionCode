ab4de1e8aa33278bb488871c1ddb015c8dd53fb9
laptop 1 <team702robotics@gmail.com>
2022-01-24 19:45:15 -0800

    Implement arcade drive

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -8,7 +8,6 @@ import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.DriveConstants;
 
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
@@ -18,7 +17,7 @@ public class DriveSubsystem extends SubsystemBase {
   private WPI_TalonSRX talonFR = new WPI_TalonSRX(DriveConstants.TALON_FR_ID);
   private CANSparkMax sparkBL = new CANSparkMax(DriveConstants.SPARK_BL_ID, MotorType.kBrushed);
   private CANSparkMax sparkBR = new CANSparkMax(DriveConstants.SPARK_BR_ID, MotorType.kBrushed);
-  private MecanumDrive drive = new MecanumDrive(talonFL, sparkBL, talonFR, sparkBR);
+  private MecanumDrive mechDrive = new MecanumDrive(talonFL, sparkBL, talonFR, sparkBR);
 
   public DriveSubsystem() {
   }
@@ -28,7 +27,7 @@ public class DriveSubsystem extends SubsystemBase {
     // Add logging of encoder values here
   }
 
-  public void driveCartesian(double ySpeed, double xSpeed, double zRotation) {
-    drive.driveCartesian(ySpeed, xSpeed, zRotation);
+  public void driveCartesian(double driveVal, double strafeVal, double rotateVal) {
+    mechDrive.driveCartesian(strafeVal, driveVal, rotateVal);
   }
 }
