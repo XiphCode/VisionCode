2f5f49a827a09a998712d29eddbfdc89ffc96927
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2022-03-05 10:47:42 -0800

    make intakes brake

--- src/main/java/frc/robot/subsystems/OuterIntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/OuterIntakeSubsystem.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -9,7 +10,9 @@ import frc.robot.commands.IntakeCommand.IntakeSubsytem;
 public class OuterIntakeSubsystem extends SubsystemBase implements IntakeSubsytem {
     private final WPI_TalonSRX talon = new WPI_TalonSRX(IntakeConstants.OUTER_TALON_ID);
 
-    public OuterIntakeSubsystem() {}
+    public OuterIntakeSubsystem() {
+        talon.setNeutralMode(NeutralMode.Brake);
+    }
 
     public void intakeSet(double val) {
         System.out.println("Set outer intake " + val);
