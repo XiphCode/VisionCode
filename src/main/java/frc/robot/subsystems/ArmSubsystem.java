172387a37d86d4428b88c332c3dc5188c1d6309b
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2022-03-05 11:43:26 -0800

    make type more unambiguous

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -3,14 +3,16 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import com.revrobotics.SparkMaxRelativeEncoder.Type;
+import com.revrobotics.SparkMaxRelativeEncoder;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ArmConstants;
 
 public class ArmSubsystem extends SubsystemBase {
     private CANSparkMax spark = new CANSparkMax(ArmConstants.ARM_SPARK, MotorType.kBrushed);
-    private RelativeEncoder encoder = spark.getEncoder(Type.kQuadrature, ArmConstants.ARM_COUNTS_PER_REV);
+    private RelativeEncoder encoder = spark.getEncoder(
+        SparkMaxRelativeEncoder.Type.kQuadrature, ArmConstants.ARM_COUNTS_PER_REV
+    );
 
     public ArmSubsystem() {} 
 
