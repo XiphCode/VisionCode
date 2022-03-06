f3592463fb3d119afc419fccd43124476b4132df
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2022-03-05 16:10:36 -0800

    climber PID dual encoder

--- src/main/java/frc/robot/commands/ClimberPIDCmd.java
+++ src/main/java/frc/robot/commands/ClimberPIDCmd.java
@@ -5,35 +5,45 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.ClimberSubsystem;
 
 public class ClimberPIDCmd extends CommandBase {
-    private ClimberSubsystem climber;
-    private PIDController controller;
+    private final ClimberSubsystem climber;
+    private final PIDController leftController, rightController;
 
     public ClimberPIDCmd(ClimberSubsystem climber, int direction) {
         this.climber = climber;
         addRequirements(climber);
+        this.leftController = getController(direction);
+        this.rightController = getController(direction);
+    }
 
-        this.controller = new PIDController(0.001, 0, 0);
-        // -1 => 0, 1 => -900
-        double halfSetpoint = -900.0 / 2.0;
-        controller.setSetpoint(direction * halfSetpoint + halfSetpoint);
-        controller.setTolerance(10);
+    private PIDController getController(int direction) {
+        PIDController c = new PIDController(0.005, 0, 0);
+        c.setSetpoint(direction == -1 ? 0.0 : 930.0);
+        c.setTolerance(10);
+        return c;
     }
 
     @Override
     public void initialize() {
-        System.out.println("ClimberControl init " + controller.getSetpoint());
+        System.out.println("ClimberControl init " + leftController.getSetpoint());
     }
 
     @Override
     public void execute() {
-        double val = controller.calculate(climber.getLeftEncoder().getPosition());
-        climber.setLeft(val);
-        System.out.println("ClimberControl execute " + val + " " + controller.getSetpoint());
+        double leftVal = 0, rightVal = 0;
+        if (!leftController.atSetpoint()) {
+            leftVal = leftController.calculate(climber.getLeftEncoder().getPosition());
+        }
+        climber.setLeft(-leftVal);
+        if (!rightController.atSetpoint()) {
+            rightVal = rightController.calculate(climber.getRightEncoder().getPosition());
+        }
+        climber.setRight(-rightVal);
+        System.out.println("ClimberControl execute " + leftVal + ", " + rightVal);
     }
 
     @Override
     public boolean isFinished() {
-        return controller.atSetpoint();
+        return leftController.atSetpoint() && rightController.atSetpoint();
     }
 
     @Override
